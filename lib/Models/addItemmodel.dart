1b3327a9c365479e146dfa1325b4a291bec3243d
Chirag Goel <chiggi03111998@gmail.com>
2020-07-15 19:58:53 +0530

    panne

---
+++ lib/Models/addItemmodel.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_tagging/flutter_tagging.dart';
+
+class AddItemsDoctorService {
+  /// Mocks fetching AddItemsDoctor from network API with delay of 500ms.
+  static Future<List<AddItemsDoctor>> getAddItemsDoctors(String query) async {
+    await Future.delayed(Duration(milliseconds: 400), null);
+    return <AddItemsDoctor>[
+      AddItemsDoctor(name: 'Asthama'),
+      AddItemsDoctor(name: 'Heart Attack'),
+      AddItemsDoctor(name: 'Cancer'),
+      AddItemsDoctor(name: 'Liver Failure'),
+      AddItemsDoctor(name: 'Durm Tore'),
+      AddItemsDoctor(name: 'Head Ache'),
+    ]
+        .where((lang) => lang.name.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+  }
+}
+
+/// AddItemsDoctor Class
+class AddItemsDoctor extends Taggable {
+  ///
+   String name;
+
+  /// Creates AddItemsDoctor
+  AddItemsDoctor({
+    this.name,
+  });
+
+  @override
+  List<Object> get props => [name];
+
+  /// Converts the class to json string.
+  String toJson() => 
+    "$name\n";
+
+}
