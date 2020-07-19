fef872d9e89cedf10d0df730f6caaf65347911dd
Ritik <ritikmiglani488@gmail.com>
2020-07-19 22:09:55 +0530

    searchbar

--- lib/Widgets/SearchBar.dart
+++ lib/Widgets/SearchBar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:getcure_doctor/Database/SymptomsTable.dart';
 import 'package:getcure_doctor/Helpers/AppConfig/colors.dart';
+import 'package:getcure_doctor/Models/PatientsVisitTableModels.dart';
 import 'package:provider/provider.dart';
 
 class SearchBar extends StatefulWidget {
@@ -13,10 +14,41 @@ class SearchBar extends StatefulWidget {
 
 class _SearchBarState extends State<SearchBar> {
   String query = '';
+  String diseaseName;
+  String _radioValue;
+  String choice;
+  void radioButtonChanges(String value) {
+    setState(() {
+      _radioValue = value;
+      switch (value) {
+        case '1month':
+          choice = value;
+          break;
+        case '3month':
+          choice = value;
+          break;
+        case '6month':
+          choice = value;
+          break;
+        case '1year':
+          choice = value;
+          break;
+        case '5year':
+          choice = value;
+          break;
+        case 'Always':
+          choice = value;
+          break;
+        default:
+          choice = null;
+      }
+      debugPrint(choice); //Debug the choice in console
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-  final database = Provider.of<SymptomsDB>(context);
+    final database = Provider.of<SymptomsDB>(context);
     return SingleChildScrollView(
       child: AlertDialog(
         title: Row(
@@ -74,7 +106,148 @@ class _SearchBarState extends State<SearchBar> {
                 right: 0,
                 child: FloatingActionButton(
                   onPressed: () {
-                    print('object');
+                    // Navigator.of(context).pop();
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return SingleChildScrollView(
+                          child: AlertDialog(
+                            titlePadding: EdgeInsets.zero,
+                            title: Container(
+                              alignment: Alignment.center,
+                              color: orangep,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 8.0, right: 8),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      'Add Disease',
+                                      style: TextStyle(color: white),
+                                    ),
+                                    IconButton(
+                                        icon: Icon(Icons.cancel),
+                                        onPressed: () => Navigator.pop(context))
+                                  ],
+                                ),
+                              ),
+                            ),
+                            actions: <Widget>[
+                              Container(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.7,
+                                width: MediaQuery.of(context).size.width * 0.8,
+                                child: Column(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.all(2.0),
+                                      child: ListTile(
+                                        title: Container(
+                                          height: 40,
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              .75,
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(10)),
+                                          child: TextFormField(
+                                            decoration: InputDecoration(
+                                              labelText: 'Enter Disease Name',
+                                              border: OutlineInputBorder(),
+                                              // suffixIcon: Icon(Icons.search)
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 10.0),
+                                    ),
+                                    Text('Visible in Brief History for'),
+                                    Container(
+                                      child: Column(
+                                        children: <Widget>[
+                                          Row(
+                                            children: <Widget>[
+                                              Radio(
+                                                  activeColor: orangep,
+                                                  value: '1month',
+                                                  groupValue: _radioValue,
+                                                  onChanged:
+                                                      radioButtonChanges),
+                                              Text('1 Month'),
+                                            ],
+                                          ),
+                                          Row(
+                                            children: <Widget>[
+                                              Radio(
+                                                  activeColor: orangep,
+                                                  value: '3month',
+                                                  groupValue: _radioValue,
+                                                  onChanged:
+                                                      radioButtonChanges),
+                                              Text('3 Months'),
+                                            ],
+                                          ),
+                                          Row(
+                                            children: <Widget>[
+                                              Radio(
+                                                  activeColor: orangep,
+                                                  value: '6month',
+                                                  groupValue: _radioValue,
+                                                  onChanged:
+                                                      radioButtonChanges),
+                                              Text('6 Months'),
+                                            ],
+                                          ),
+                                          Row(
+                                            children: <Widget>[
+                                              Radio(
+                                                  activeColor: orangep,
+                                                  value: '1year',
+                                                  groupValue: _radioValue,
+                                                  onChanged:
+                                                      radioButtonChanges),
+                                              Text('1 Year'),
+                                            ],
+                                          ),
+                                          Row(
+                                            children: <Widget>[
+                                              Radio(
+                                                  activeColor: orangep,
+                                                  value: '5year',
+                                                  groupValue: _radioValue,
+                                                  onChanged:
+                                                      radioButtonChanges),
+                                              Text('5 Years'),
+                                            ],
+                                          ),
+                                          Row(
+                                            children: <Widget>[
+                                              Radio(
+                                                  activeColor: orangep,
+                                                  value: 'Always',
+                                                  groupValue: _radioValue,
+                                                  onChanged:
+                                                      radioButtonChanges),
+                                              Text('Always'),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
                   },
                   child: Icon(
                     Icons.add,
